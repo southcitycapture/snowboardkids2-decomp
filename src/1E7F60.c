633ac419dade13e181852d0f19f92dec18b34ef9
Chris Lewis <me@chrislewis.au>
2026-08-28 23:24:09 -0400

    Decompile func_800BB058_1E80A8

--- src/1E7F60.c
+++ src/1E7F60.c
@@ -1,4 +1,8 @@
 #include "common.h"
+#include "system/task_scheduler.h"
+
+void func_800BB084_1E80D4(void);
+void func_800BB1CC_1E821C(void);
 
 INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BAF10_1E7F60);
 
@@ -14,7 +18,10 @@ INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB040_1E8090);
 
 INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB04C_1E809C);
 
-INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB058_1E80A8);
+void func_800BB058_1E80A8(void) {
+    setCleanupCallback(&func_800BB1CC_1E821C);
+    setCallback(&func_800BB084_1E80D4);
+}
 
 INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB084_1E80D4);
 
