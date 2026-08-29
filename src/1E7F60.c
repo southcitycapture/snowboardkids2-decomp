f30e27af2d92d57f74248edfd0c7783f18f2cea5
Chris Lewis <me@chrislewis.au>
2026-08-29 00:48:50 -0400

    Decompile func_800BAF10_1E7F60

--- src/1E7F60.c
+++ src/1E7F60.c
@@ -12,10 +12,40 @@ typedef struct {
     u8 state;
 } ColorRectState;
 
+typedef struct {
+    ColorRectState *unk0;
+    s32 unk4;
+} ColorRectTask;
+
+void func_800BAFF0_1E8040(ColorRectState *, s16, s16, u16, u16);
+void func_800BB028_1E8078(ColorRectState *, u8, u8, u8);
+void func_800BB058_1E80A8(void);
 void func_800BB084_1E80D4(void);
 void func_800BB1CC_1E821C(void);
 
-INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BAF10_1E7F60);
+void func_800BAF10_1E7F60(
+    ColorRectState *arg0,
+    s32 arg1,
+    s16 arg2,
+    s16 arg3,
+    u16 arg4,
+    u16 arg5,
+    u8 arg6,
+    u8 arg7,
+    u8 arg8
+) {
+    s32 sp1C;
+    ColorRectTask *temp_v0;
+
+    sp1C = arg1;
+    temp_v0 = scheduleTask(func_800BB058_1E80A8, 0U, 0U, 0x64U);
+    if (temp_v0 != NULL) {
+        func_800BAFF0_1E8040(arg0, arg2, arg3, arg4, (u16)(s32)arg5);
+        func_800BB028_1E8078(arg0, arg6, arg7, arg8);
+        temp_v0->unk0 = arg0;
+        temp_v0->unk4 = sp1C;
+    }
+}
 
 void func_800BAFF0_1E8040(ColorRectState *arg0, s16 arg1, s16 arg2, u16 arg3, u16 arg4) {
     arg0->x = arg1;
