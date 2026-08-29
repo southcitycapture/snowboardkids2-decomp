97f60910f024cef00c75b6e2cc08a1fd6d4ecb47
Chris Lewis <me@chrislewis.au>
2026-08-29 01:02:12 -0400

    Decompile func_800BB084_1E80D4

--- src/1E7F60.c
+++ src/1E7F60.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "graphics/graphics.h"
 #include "system/task_scheduler.h"
 
 typedef struct {
@@ -13,19 +14,21 @@ typedef struct {
 } ColorRectState;
 
 typedef struct {
-    ColorRectState *unk0;
-    s32 unk4;
+    ColorRectState *state;
+    ViewportNode *viewport;
+    ColorRectState rects[4];
 } ColorRectTask;
 
 void func_800BAFF0_1E8040(ColorRectState *, s16, s16, u16, u16);
 void func_800BB028_1E8078(ColorRectState *, u8, u8, u8);
 void func_800BB058_1E80A8(void);
-void func_800BB084_1E80D4(void);
+void func_800BB084_1E80D4(ColorRectTask *task);
 void func_800BB1CC_1E821C(void);
+void drawColorRect(ColorRectState *rect);
 
 void func_800BAF10_1E7F60(
     ColorRectState *arg0,
-    s32 arg1,
+    ViewportNode *arg1,
     s16 arg2,
     s16 arg3,
     u16 arg4,
@@ -34,16 +37,16 @@ void func_800BAF10_1E7F60(
     u8 arg7,
     u8 arg8
 ) {
-    s32 sp1C;
+    ViewportNode *viewport;
     ColorRectTask *temp_v0;
 
-    sp1C = arg1;
+    viewport = arg1;
     temp_v0 = scheduleTask(func_800BB058_1E80A8, 0U, 0U, 0x64U);
     if (temp_v0 != NULL) {
         func_800BAFF0_1E8040(arg0, arg2, arg3, arg4, (u16)(s32)arg5);
         func_800BB028_1E8078(arg0, arg6, arg7, arg8);
-        temp_v0->unk0 = arg0;
-        temp_v0->unk4 = sp1C;
+        temp_v0->state = arg0;
+        temp_v0->viewport = viewport;
     }
 }
 
@@ -84,7 +87,38 @@ void func_800BB058_1E80A8(void) {
     setCallback(&func_800BB084_1E80D4);
 }
 
-INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB084_1E80D4);
+void func_800BB084_1E80D4(ColorRectTask *task) {
+    s32 i;
+    ColorRectState *state;
+
+    state = task->state;
+    if (state->state != 1) {
+        task->rects[0].x = state->x;
+        task->rects[0].y = state->y;
+        task->rects[0].width = state->width - 1;
+        task->rects[0].height = 1;
+        task->rects[1].x = state->x + state->width - 1;
+        task->rects[1].y = state->y;
+        task->rects[1].width = 1;
+        task->rects[1].height = state->height - 1;
+        task->rects[2].x = state->x;
+        task->rects[2].y = state->y + state->height - 1;
+        task->rects[2].width = state->width - 1;
+        task->rects[2].height = 1;
+        task->rects[3].x = state->x;
+        task->rects[3].y = state->y;
+        task->rects[3].width = 1;
+        task->rects[3].height = state->height - 1;
+
+        for (i = 0; i < 4; i++) {
+            task->rects[i].red = state->red;
+            task->rects[i].green = state->green;
+            task->rects[i].blue = state->blue;
+            task->rects[i].state = 0;
+            pushViewportCallbackBySlot(task->viewport->callbackSlotIndex, 7, drawColorRect, &task->rects[i]);
+        }
+    }
+}
 
 void func_800BB1CC_1E821C(void) {
 }
