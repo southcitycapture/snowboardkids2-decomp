abca78884ce7741c691abae9971d72885649a728
Chris Lewis <me@chrislewis.au>
2026-08-29 00:07:42 -0400

    Decompile func_800BB1F8_1E8248

--- src/1E8240.c
+++ src/1E8240.c
@@ -16,7 +16,14 @@ typedef struct {
     /* 0x0A */ s16 y;
     /* 0x0C */ u16 columnCount;
     /* 0x0E */ u16 rowCount;
-    /* 0x10 */ u8 padding10[0x18];
+    /* 0x10 */ s16 cursorX;
+    /* 0x12 */ s16 cursorY;
+    /* 0x14 */ s16 unk14;
+    /* 0x16 */ u8 padding16[2];
+    /* 0x18 */ char *text;
+    /* 0x1C */ char textBuffer[8];
+    /* 0x24 */ s16 scrollX;
+    /* 0x26 */ s16 scrollY;
     /* 0x28 */ u8 enabled;
     /* 0x29 */ u8 stopRequested;
 } CutsceneEditorTextGrid;
@@ -27,11 +34,43 @@ typedef struct {
     /* 0x04 */ CutsceneEditorTextGrid *grid;
 } CutsceneEditorTextGridTask;
 
+void func_800BB5D4_1E8624(CutsceneEditorTextGridTask *task);
+
 u8 func_800BB1F0_1E8240(CutsceneEditorTextGrid *grid) {
     return grid->enabled;
 }
 
-INCLUDE_ASM("asm/nonmatchings/1E8240", func_800BB1F8_1E8248);
+void func_800BB1F8_1E8248(CutsceneEditorTextGrid *grid, void *context, s16 x, s16 y, u16 columnCount, u16 rowCount) {
+    CutsceneEditorTextGridTask *task;
+
+    task = (CutsceneEditorTextGridTask *)scheduleTask(&func_800BB5D4_1E8624, 0, 0, 0x65);
+    if (task != NULL) {
+        grid->unk14 = 5;
+        grid->text = grid->textBuffer;
+        grid->textBuffer[0] = '*';
+        grid->textBuffer[2] = ' ';
+        grid->textBuffer[3] = ' ';
+        grid->textBuffer[4] = ' ';
+        grid->textBuffer[5] = ' ';
+        grid->textBuffer[6] = ' ';
+        grid->context = context;
+        grid->x = x;
+        grid->y = y;
+        grid->columnCount = columnCount;
+        grid->rowCount = rowCount;
+        grid->cursorX = 0;
+        grid->cursorY = 0;
+        grid->textBuffer[1] = '\0';
+        grid->textBuffer[7] = '\0';
+        grid->scrollX = 0;
+        grid->scrollY = 0;
+        grid->enabled = TRUE;
+        grid->stopRequested = FALSE;
+        grid->entries = NULL;
+        task->initialized = FALSE;
+        task->grid = grid;
+    }
+}
 
 void func_800BB2E8_1E8338(CutsceneEditorTextGrid *grid) {
     grid->stopRequested = TRUE;
