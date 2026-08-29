2eed6ff618ca8d1309d67a02283cc542210dd030
Chris Lewis <me@chrislewis.au>
2026-08-28 23:41:55 -0400

    Decompile func_800BB040_1E8090

--- src/1E7F60.c
+++ src/1E7F60.c
@@ -1,6 +1,17 @@
 #include "common.h"
 #include "system/task_scheduler.h"
 
+typedef struct {
+    u16 x;
+    u16 y;
+    u16 width;
+    u16 height;
+    u8 red;
+    u8 green;
+    u8 blue;
+    u8 state;
+} ColorRectState;
+
 void func_800BB084_1E80D4(void);
 void func_800BB1CC_1E821C(void);
 
@@ -14,7 +25,9 @@ INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB028_1E8078);
 
 INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB038_1E8088);
 
-INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB040_1E8090);
+void func_800BB040_1E8090(ColorRectState *arg0) {
+    arg0->state = 1;
+}
 
 INCLUDE_ASM("asm/nonmatchings/1E7F60", func_800BB04C_1E809C);
 
